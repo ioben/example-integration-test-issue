21fa46b614f6b18d4f4885307828784e42afb07c
ben <ben@ros.io>
2019-01-16 17:09:33 -0700

    Use lambdas instead of anonymous class
    Anonymous class creates new instance per instantiation, causing
    MergedContextConfiguration to be different between contexts when it'd
    otherwise be equivilant. Use lambdas instead, which don't create
    fullblown classes, and therefore don't exhibit this issue.

--- src/test/java/com/example/testcontainers/CloseParentContextCustomizerFactory.java
+++ src/test/java/com/example/testcontainers/CloseParentContextCustomizerFactory.java
@@ -1,13 +1,10 @@
 package com.example.testcontainers;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.test.context.ContextConfigurationAttributes;
 import org.springframework.test.context.ContextCustomizer;
 import org.springframework.test.context.ContextCustomizerFactory;
-import org.springframework.test.context.MergedContextConfiguration;
 
 import java.util.List;
 
@@ -16,19 +13,10 @@ public class CloseParentContextCustomizerFactory implements ContextCustomizerFac
     public ContextCustomizer createContextCustomizer(
             Class<?> testClass,
             List<ContextConfigurationAttributes> configAttributes) {
-        return new ContextCustomizer() {
-            @Override
-            public void customizeContext(ConfigurableApplicationContext context,
-                                         MergedContextConfiguration mergedConfig) {
-                context.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
-                    @Override
-                    public void onApplicationEvent(ContextClosedEvent event) {
-                        ApplicationContext parentContext = event.getApplicationContext().getParent();
-                        if (parentContext.getId().equals("bootstrap"))
-                            ((ConfigurableApplicationContext) parentContext).close();
-                    }
-                });
-            }
-        };
+        return (context, mergedConfig) -> context.addApplicationListener((ContextClosedEvent event) -> {
+            ConfigurableApplicationContext parentContext = (ConfigurableApplicationContext)event.getApplicationContext().getParent();
+            if (parentContext != null && parentContext.getId().equals("bootstrap"))
+                parentContext.close();
+        });
     }
 }
